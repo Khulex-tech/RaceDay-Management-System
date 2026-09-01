52c13cca3cba2e0e277f45221d4d6042b007570f
Khulex-tech <thapelomkhari791@gmail.com>
2026-09-01 21:11:38 +0200

    Add RaceDay database creation script to initialize database and tables with sample data.

---
+++ docs/RaceDay_DatabaseScript.sql
@@ -0,0 +1,19 @@
+-- RaceDay Database Script
+-- This script creates the RaceDay database, all eight tables and the sample data
+
+-- Creating the database
+USE master;
+GO
+
+IF DB_ID('RaceDayDb') IS NOT NULL
+BEGIN
+    ALTER DATABASE RaceDayDb SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE RaceDayDb;
+END
+GO
+
+CREATE DATABASE RaceDayDb;
+GO
+
+USE RaceDayDb;
+GO
